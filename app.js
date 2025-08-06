7ad3802f6f437f79d950fef8292592f88cfee0c4
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2025-08-06 18:45:52 -0500

    Apply recursion in the code to not allow repeated secret friends

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var friendList = []; //Array para almacenar los amigos
+var noRepeated = []; //Array para que no salga el mismo firend dos veces
 
 
 function addFriend(){ 
@@ -32,25 +33,40 @@ function actualizeFriendList(){
 
 
 function randomIndex(){
-    let randomListIndex = Math.floor(Math.random() * friendList.length);
+    let randomListIndex = Math.floor(Math.random() * friendList.length); //Generar un indice aleatorio
     return randomListIndex
 }
 
-function drawnFriend(){
-    const div = document.querySelector('.input-section') 
-    if(friendList.length < 2 ){
+function drawnFriend() {
+    const div = document.querySelector('.input-section');
+    if (friendList.length < 2) {
         alert('The list must contain at least two friends to be drawn.');
-    } else {
-        let secretFriend = friendList[randomIndex()];
-        console.log(secretFriend);
-        const p = document.createElement('h1');
-        p.textContent = `You're secret friend is ${secretFriend}`;
-        div.appendChild(p);
+        return;
+    }
+    //Sortear un amigo secreto
+    let secretFriend = friendList[randomIndex()];
+
+    //Si ya se sprtio, repite la accion por favor !!!(recurción)
+    if (noRepeated.includes(secretFriend)) {
+        //Si todos los amigos ya han sido sorteados, mostrar un mensaje
+        if (noRepeated.length === friendList.length) {
+            alert('All friends have already been drawn!');
+            return;
+        }
+        return drawnFriend();
     }
 
+    //Agregar el amigo secreto al array de no repetidos
+    noRepeated.push(secretFriend);
+
+    //Crear un elemento h1 para mostrar el amigo secreto
+    const p = document.createElement('h1');
+    p.textContent = `Your secret friend is ${secretFriend}`;
+    div.appendChild(p);
 }
 
 
 
-console.log(friendList)
+
+console.log(friendList)//Para ver el contenido del array en la consola
 
