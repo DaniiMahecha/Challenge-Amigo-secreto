fd89162f7514ab85a2e1a5aacaf8b5fdd08875aa
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2025-08-05 19:09:27 -0500

    addFriend() Function added

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+var friendList = [];
+
+function addFriend(){
+    //Si el espacio esta vacio lanzar un alert sino
+    let friend = document.getElementById("amigo");
+    let friendName = friend.value.trim()
+    
+    if(friendName != ''){
+        friendList.push(friendName); //Agregar el dato al Array 
+        friend.value = ''; //Limpiar el Array  
+    } else {
+        alert('Indique un nombre');            
+    }  
+}
+
+console.log(friendList)
+    
+    
